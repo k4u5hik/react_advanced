a2400dc502ac5b71bcde32ddf5206feb12c5ece9
Kaushik Chemburkar <attherate@gmail.com>
2022-02-11 14:31:31 +1100

    now can click button to change the title value from random title to hello world

--- src/tutorial/1-useState/practice/2-useState-basics.js
+++ src/tutorial/1-useState/practice/2-useState-basics.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react'
 
 const UseStateBasics = () => {
-  console.log(useState('hello world'))
-  const value = useState(1)[0]
-  const handler = useState(1)[1]
-  console.log(value, handler)
-  return <h2>useState basic example</h2>
+  // console.log(useState('hello world'))
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
+
+  const [text, setText] = useState('random title')
+  const handleClick = () => {
+    setText('hello world')
+  }
+
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className='btn' onClick={handleClick}>
+        Change title
+      </button>
+    </React.Fragment>
+  )
 }
 
 export default UseStateBasics
