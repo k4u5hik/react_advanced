3c9591090753c6f2cae30bf6c2da7a862f8e5d0d
Kaushik Chemburkar <attherate@gmail.com>
2022-02-13 22:08:02 +1100

    useEffect - are works/processes outside of the component ... could be like changing doc title, fetching data, signing up for subscription, event listener etc.
    - checking the console log
    - render component is logged twice due to <React.StrictMode> in the index.js

--- src/tutorial/2-useEffect/practice/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/practice/1-useEffect-basics.js
@@ -1,9 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
+  useEffect(() => {
+    console.log('call useEffect')
+  })
+  console.log('render component')
+  return <h2>useEffect Basics</h2>
+}
 
-export default UseEffectBasics;
+export default UseEffectBasics
