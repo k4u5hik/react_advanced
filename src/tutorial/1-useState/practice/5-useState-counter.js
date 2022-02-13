e056e5acb48278ba043b6f2ccdd9a79b5d1de74d
Kaushik Chemburkar <attherate@gmail.com>
2022-02-13 13:57:33 +1100

    Functional Update Form - part 1
    - when i click increase later, the values do not update corresponding to the number of times the button has been clicked.

--- src/tutorial/1-useState/practice/5-useState-counter.js
+++ src/tutorial/1-useState/practice/5-useState-counter.js
@@ -5,6 +5,11 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0)
   }
+  const complexIncrease = () => {
+    setTimeout(() => {
+      setValue(value + 1)
+    }, 2000)
+  }
   return (
     <>
       <section style={{ margin: '4rem 0' }}>
@@ -18,6 +23,14 @@ const UseStateCounter = () => {
         </button>
         <button className='btn' onClick={() => setValue(value - 1)}>
           decrease
+        </button>
+      </section>
+
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>
+          increase later
         </button>
       </section>
     </>
