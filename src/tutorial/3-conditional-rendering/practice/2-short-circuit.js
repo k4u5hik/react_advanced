2f511ff72173420b7b777e5b6c9cea1866990bab
Kaushik Chemburkar <attherate@gmail.com>
2022-02-14 17:45:59 +1100

    short circuit && || !  (truthy || falsey) conditional(ternary) ? :

--- src/tutorial/3-conditional-rendering/practice/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/practice/2-short-circuit.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('K')
 
-  return <h2>short circuit</h2>;
-};
+  const firstValue = text || 'hello world'
+  const secondValue = text && 'hello world'
 
-export default ShortCircuit;
+  return (
+    <>
+      <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1>
+    </>
+  )
+}
+
+export default ShortCircuit
