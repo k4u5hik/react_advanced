3b955ca61ad094600e010c3cb10b4aeba56807f7
Kaushik Chemburkar <attherate@gmail.com>
2022-02-24 13:13:23 +1100

    useReducer setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/5-useRef/practice/1-useRef-basics'
+import Setup from './tutorial/6-useReducer/practice/index'
 
 function App() {
   return (
