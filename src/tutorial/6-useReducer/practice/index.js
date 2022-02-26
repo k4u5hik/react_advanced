51c46e206843c6829fd2ac1f376729e3edd0cc41
Kaushik Chemburkar <attherate@gmail.com>
2022-02-26 22:02:15 +1100

    remove item complete - useReducer refactored

--- src/tutorial/6-useReducer/practice/index.js
+++ src/tutorial/6-useReducer/practice/index.js
@@ -2,25 +2,7 @@ import React, { useState, useReducer } from 'react'
 import Modal from './Modal'
 import { data } from '../../../data'
 // reducer function
-
-const reducer = (state, action) => {
-  if (action.type === 'ADD_ITEM') {
-    const newPeople = [...state.people, action.payload]
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: 'Item added successfully',
-    }
-  }
-  if (action.type === 'NO_VALUE') {
-    return { ...state, isModalOpen: true, modalContent: 'Please enter a value' }
-  }
-  if (action.type === 'CLOSE_MODAL') {
-    return { ...state, isModalOpen: false }
-  }
-  throw new Error('no matching action type')
-}
+import { reducer } from './reducer'
 
 const defaultState = {
   people: [],
