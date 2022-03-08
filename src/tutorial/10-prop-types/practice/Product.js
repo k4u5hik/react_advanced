5e5231499c2eff70b9f645635190e7ff98b7b9a9
Kaushik Chemburkar <attherate@gmail.com>
2022-03-08 20:04:00 +1100

    How to try to troubleshoot the error - single product is showing -inspect you will find undefined in the errors

--- src/tutorial/10-prop-types/practice/Product.js
+++ src/tutorial/10-prop-types/practice/Product.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Product = ({ image, name, price }) => {
   return (
     <article className='product'>
-      <img src={image.url} alt={name} />
+      <h4>single product</h4>
+      {/* <img src={image.url} alt={name} />
       <h4>{name}</h4>
-      <p>{price}</p>
+      <p>{price}</p> */}
     </article>
   )
 }
