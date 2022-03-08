2b18301af743f6293111340d6bac29be8b0a9c84
Kaushik Chemburkar <attherate@gmail.com>
2022-03-08 20:06:28 +1100

    using proptypes and inspector

--- src/tutorial/10-prop-types/practice/Product.js
+++ src/tutorial/10-prop-types/practice/Product.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Product = ({ image, name, price }) => {
   return (
@@ -9,6 +10,12 @@ const Product = ({ image, name, price }) => {
       <p>{price}</p> */}
     </article>
   )
+}
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
 }
 
 export default Product
