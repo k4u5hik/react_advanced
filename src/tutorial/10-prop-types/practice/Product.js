b659b888c0fc6b2b4fcaa49ee3cadb32e3019edf
Kaushik Chemburkar <attherate@gmail.com>
2022-03-08 20:15:37 +1100

    Another way to set default props - if image is not there then we are not checking it and then display the defaultImage - use or for default price. - this is a bit messy method and makes the file bigger and harder to read.

--- src/tutorial/10-prop-types/practice/Product.js
+++ src/tutorial/10-prop-types/practice/Product.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types'
 import defaultImage from '../../../assets/default-image.jpeg'
 
 const Product = ({ image, name, price }) => {
+  const url = image && image.url
   return (
     <article className='product'>
       {/* <h4>single product</h4> */}
-      <img src={image.url} alt={name} />
+      <img src={url || defaultImage} alt={name || 'default name'} />
       <h4>{name}</h4>
-      <p>{price}</p>
+      <p>{price || 0.0}</p>
     </article>
   )
 }
@@ -18,9 +19,10 @@ Product.propTypes = {
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
 }
-Product.defaultProps = {
-  name: 'no name',
-  price: 0.0,
-  image: defaultImage,
-}
+// Product.defaultProps = {
+//   name: 'no name',
+//   price: 0.0,
+//   image: defaultImage,
+// }
+
 export default Product
